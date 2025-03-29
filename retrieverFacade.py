e46168f168aadcc4fb556572902c7a9639adb183
Parham Abdolmohammadi <parham.abdo@gmail.com>
2025-03-28 17:54:19 -0700

    complying retriever facade with retrievers

--- retrieverFacade.py
+++ retrieverFacade.py
@@ -2,6 +2,7 @@ import requests
 
 from pokedex_object import PokedexObject
 from request import Request
+from retriever import PokemonRetriever, AbilityRetriever, MoveRetriever, Retriever
 
 
 class PokedexRetrieverFacade:
@@ -9,32 +10,32 @@ class PokedexRetrieverFacade:
     BASE_URL = "https://pokeapi.co/api/v2"
 
     RETRIEVERS: dict[str, tuple[str, str]] = {
-        "pokemon": ("PokemonRetriever", BASE_URL + "pokemon/{}"),
-        "ability": ("AbilityRetriever", BASE_URL + "ability/{}"),
-        "move": ("MoveRetriever", BASE_URL + "move/{}")
+        "pokemon": (PokemonRetriever(), BASE_URL + "pokemon/{}"),
+        "ability": (AbilityRetriever(), BASE_URL + "ability/{}"),
+        "move": (MoveRetriever(), BASE_URL + "move/{}")
     }
 
 
     def __init__(self, requests: list[Request]):
         self.requests = requests
         self.pokedex_data: list[PokedexObject] = []
-        self.retriever = None
+        self.retriever : Retriever = None
 
 
 
-    def execute_request(self, request: list[Request]) -> list[PokedexObject]:
+    def execute_request(self, requests: list[Request]):
+
+        mode = requests[0].mode
+        retriever_information =  self.RETRIEVERS[mode]
+        self.retriever = retriever_information[0]
+        url = retriever_information[1]
 
 
-        for request in requests:
-            retriever_info = PokedexRetrieverFacade.RETRIEVERS.get(request.mode)
-
-            self.retriever = retriever_info[0]
-            url = retriever_info[1]
+        self.pokedex_data =  self.retriever.fetch_data(url, requests)
 
 
 
-            # current_data = retriever.executerequest(url, request.input_data)
-            #self.pokedex_data.apppend(current_data)
+
 
 
 
